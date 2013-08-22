a6c534bf9e1c4bf1703b661b3f95428cb8f0d020
BjoernAkAManf <muell.mail.d@googlemail.com>
2013-08-22 12:02:37 +0200

    Adding command for saving Player

--- src/main/java/tk/manf/InventorySQL/InventorySQLPlugin.java
+++ src/main/java/tk/manf/InventorySQL/InventorySQLPlugin.java
@@ -1,34 +1,28 @@
 /**
  * Copyright (c) 2013 Exo-Network
- * 
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
- * 
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- * 
- *    1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 
- *    2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 
- *    3. This notice may not be removed or altered from any source
- *    distribution.
- * 
+ *
+ * This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product
+ * documentation would be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
  * manf                   info@manf.tk
  */
 
 package tk.manf.InventorySQL;
 
 import java.io.IOException;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.Metrics;
 import tk.manf.InventorySQL.manager.ConfigManager;
@@ -105,8 +99,28 @@ public class InventorySQLPlugin extends JavaPlugin {
                     return;
                 }
             }
+            if (args[0].equalsIgnoreCase("save")) {
+                Player p;
+                if (args.length > 1) {
+                    p = Bukkit.getPlayer(args[1]);
+                    if (p == null) {
+                        sender.sendMessage("Sorry, but this Player is not online!");
+                        return;
+                    }
+                } else {
+                    if (sender instanceof Player) {
+                        p = (Player) sender;
+                    } else {
+                        sender.sendMessage("Sorry, but you cannot be saved!");
+                        return;
+                    }
+                }
+                DatabaseManager.getInstance().savePlayer(p);
+                sender.sendMessage("Player " + p.getName() + " has been saved!");
+                return;
+            }
         }
         sender.sendMessage("/" + label + " reload " + "[LANGUAGE||CONFIG]");
+        sender.sendMessage("/" + label + " save");
     }
-
 }
